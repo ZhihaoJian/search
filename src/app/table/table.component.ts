c29ff8c44bd209e50da00c82da3ecb40ad9a73db
ZhihaoJian <jianzhihao123@gmail.com>
2017-10-19 16:17:38 +0800

    新增分页面板范围限定

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -13,6 +13,7 @@ export class TableComponent implements OnInit, AfterViewInit {
   private totalPage = 0;
   private resultsLength = 0;
   @ViewChild('currentPage') currentPage: ElementRef;
+  @ViewChild('pageSize') pageSize: ElementRef;
 
   @Input()
   set gridContentArray(gca: Array<object>) {
@@ -39,19 +40,42 @@ export class TableComponent implements OnInit, AfterViewInit {
    */
   onChangePage(direction) {
 
+    // 改变分页面板分页值
+    if (direction === 'first-page') {
+      this.currentPage.nativeElement.value = 1;
+    } else if (direction === 'next-page') {
+      if (parseInt(this.currentPage.nativeElement.value, 10) + 1 > this.totalPage) {
+        return;
+      }
+      this.currentPage.nativeElement.value = parseInt(this.currentPage.nativeElement.value, 10) + 1;
+
+    } else if (direction === 'last-page') {
+      this.currentPage.nativeElement.value = this.totalPage;
+    } else {
+      if (this.currentPage.nativeElement.value - 1 <= 0) {
+        return;
+      }
+      this.currentPage.nativeElement.value = parseInt(this.currentPage.nativeElement.value, 10) - 1;
+    }
+
+    // 获取下拉框的参数（含有当前门类的tableName和cataId）
     const selection = $('#catalogueSelection')[0];
     const selectedIndex = selection.selectedIndex;
     const params = selection.selectedOptions[selectedIndex].id;
 
-    if (direction === 'first-page') {
-      this.currentPage.nativeElement.value = 1;
-    } else if (direction === 'next-page') {
-      this.currentPage.nativeElement.value = parseInt(this.currentPage.nativeElement.value, 10) + 1;
-    } else if (direction === 'last-page') {
-      this.currentPage.nativeElement.value = this.totalPage;
-    } else {
-      this.currentPage.nativeElement.value = parseInt(this.currentPage.nativeElement.value, 10) - 1;
-    }
+    const index = params.indexOf('&');
+    const tableName = params.substring(0, index);
+    const cataId = params.substring(index + 1);
+
+    // 获取pageSize
+    const pageSize = this.pageSize.nativeElement.selectedOptions[this.pageSize.nativeElement.options['selectedIndex']].value
+
+    this.cfs.getFirstSelectionGrid(
+      '/app/appController/loadDataForTableHeader',
+      tableName,
+      cataId,
+      this.currentPage.nativeElement.value,
+      pageSize);
   }
 
 }
